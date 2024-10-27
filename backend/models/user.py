30bb5a178a4817c1d60708a7ed55e3af0c8c0925
Owen Lin <106612301+owenowenisme@users.noreply.github.com>
2024-10-28 02:24:55 +0800

    Dev/backend@0.0.0 (#5)
    * remove golang
    
    * Init fastapi (#11)
    
    * inited fastapi
    
    * updated .gitignore and added template .env file
    
    * remove unused script in poetry
    
    * add simple Readme
    
    * update Readme
    
    * added read different .env files based on command line args & update dockerfile

---
+++ backend/models/user.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, relationship
+
+from .base import Base, BaseModel
+
+if TYPE_CHECKING:
+    from .file import File
+
+
+class User(Base):
+    __tablename__ = 'Users'
+    userId: Mapped[BaseModel.int_primary_key]
+    username: Mapped[BaseModel.str_base]
+    password: Mapped[BaseModel.str_base]
+    email: Mapped[BaseModel.str_base]
+    timestamp: Mapped[BaseModel.timestamp]
+    files: Mapped[list['File']] = relationship(
+        'File',  # type: ignore
+        back_populates='uploader',
+        cascade='all, delete-orphan',
+        lazy='select',
+        order_by='File.filename',
+    )
+
+    def __init__(self, username: str, password: str, email: str):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.timestamp = datetime.now()
+        self.files = []
+
+    def __repr__(self):
+        return f'User(username={self.username}, email={self.email})'
