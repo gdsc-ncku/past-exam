a41cdb631f613944af6c02691bc875623b6e3c75
Max042004 <sau525@gmail.com>
2024-11-17 10:50:42 +0800

    Refactor: move repetitive code to separate file

---
+++ frontend/components/GlobalNav/Link.ts
@@ -0,0 +1,14 @@
+export const LinkItem = [
+  {
+    href: '/Files',
+    label: 'Files'
+  },
+  {
+    href: '/upload',
+    label: 'Upload'
+  },
+  {
+    href: '/Search',
+    label: 'Search'
+  }
+];
