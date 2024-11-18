cdf8b61c17f01f0a4b883666f56f0380cda20ecc
Dennis <e64112287@gs.ncku.edu.tw>
2024-11-18 15:00:22 +0800

    adjust response model

--- backend/routers/comment.py
+++ backend/routers/comment.py
@@ -1,30 +1,47 @@
 from typing import List
 
 from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import JSONResponse
 
 from crud.comment import CommentCRUD
 from schemas.comment import CommentCreate, CommentResponse
+from schemas.common import CommentResponseModel
+from utils.comment import serialize_datetime as ser
 
 router = APIRouter(tags=['comment'], prefix='/comment')
 
 
 @router.post(
     '',
-    response_model=CommentResponse,
+    response_model=CommentResponseModel[CommentResponse],
     status_code=status.HTTP_201_CREATED,
     response_description='Create a new comment',
 )
 async def create_comment(comment: CommentCreate):
     try:
         db_comment = CommentCRUD.create_comment(comment)
-        return CommentResponse(
+        data = CommentResponse(
             commenter_id=db_comment.commenter_id,
             comment_time=db_comment.comment_time,
             content=db_comment.content,
             comment_id=db_comment.comment_id,
         )
+        return CommentResponseModel(status='success', message=None, data=data)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'An error occurred: {str(e)}')
+        error_response = CommentResponseModel(
+            status='error',
+            message=str(e),
+            data=None,  # No data in error case
+        )
+
+        if error_response and hasattr(error_response, 'timestamp'):
+            error_response.timestamp = ser(error_response.timestamp)
+
+        # You can control the status code here
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,  # or any other status you want
+            content=error_response.model_dump(),  # Convert the response model to a dictionary
+        )
 
 
 @router.get('', response_model=List[CommentResponse], status_code=status.HTTP_200_OK)
--- backend/schemas/common.py
+++ backend/schemas/common.py
@@ -1,17 +1,28 @@
 from datetime import datetime
-from enum import Enum
-from typing import Generic, Optional, TypeVar
+from typing import Generic, Literal, Optional, TypeVar, Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 T = TypeVar('T')
 
-class ResponseStatus(Enum):
-    SUCCESS = "success"
-    ERROR = "error"
 
-class ResponseModel(BaseModel, Generic[T]):
-    status: ResponseStatus
+class CommentResponseModel(BaseModel, Generic[T]):
+    status: Literal['success', 'error']
     message: Optional[str] = None
     data: Optional[T] = None
-    timestamp: datetime = Field(default_factory=datetime.now)
+    timestamp: Union[datetime, str] = datetime.now()
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'status': 'success',
+                'message': None,
+                'data': {
+                    'comment_id': 1,
+                    'commenter_id': 'dennislee03',
+                    'content': 'Hello World!',
+                    'comment_time': '2024-11-18T03:18:41.691Z',
+                },
+                'timestamp': '2024-11-18T13:42:22.308579',
+            }
+        }
