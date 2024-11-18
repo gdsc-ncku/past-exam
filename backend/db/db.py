605b9007f3b07098e33dbd904a5c2cdfb15bb212
Dennis <e64112287@gs.ncku.edu.tw>
2024-11-18 11:24:04 +0800

    rebase task/dennis/comment-module

--- backend/db/db.py
+++ backend/db/db.py
@@ -4,31 +4,30 @@ from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 from core.config import get_settings
+from models.comment import Comment
 from models.file import File
 from models.user import User
 
-Base = declarative_base()
 
-settings = get_settings()
-DATABASE_URL = URL.create(
-    'postgresql',
-    username=settings.postgres_user,
-    password=settings.postgres_password,
-    host=settings.postgres_ip,
-    port=settings.postgres_port,
-    database=settings.postgres_db,
-)
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def get_db() -> Generator[Session, None, None]:
+def get_db():
     db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    return db
 
 
-def init_db() -> None:
-    Base.metadata.create_all(bind=engine, tables=[User.__table__, File.__table__])
+def init_db():
+    settings = get_settings()
+    DATABASE_URL = URL.create(
+        'postgresql',
+        username=settings.postgres_user,
+        password=settings.postgres_password,
+        host=settings.postgres_ip,
+        port=settings.postgres_port,
+        database=settings.postgres_db,
+    )
+    engine = create_engine(DATABASE_URL)
+    global SessionLocal
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base = declarative_base()
+    Base.metadata.create_all(
+        bind=engine, tables=[User.__table__, File.__table__,Comment.__table__]
+    )
---
+++ backend/routers/comment.py
@@ -0,0 +1,76 @@
+from typing import List
+
+from fastapi import APIRouter, HTTPException, status
+
+from crud.comment import CommentCRUD
+from schemas.comment import CommentCreate, CommentResponse
+
+router = APIRouter(tags=['comment'], prefix='/comment')
+
+
+@router.post(
+    '/',
+    response_model=CommentResponse,
+    status_code=status.HTTP_201_CREATED,
+    response_description='Create a new comment',
+)
+async def create_comment(comment: CommentCreate):
+    try:
+        db_comment = CommentCRUD.create_comment(comment)
+        return CommentResponse(
+            commenter_id=db_comment.commenter_id,
+            comment_time=db_comment.comment_time,
+            content=db_comment.content,
+            comment_id=db_comment.comment_id,
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'An error occurred: {str(e)}')
+
+
+@router.get('/', response_model=List[CommentResponse], status_code=status.HTTP_200_OK)
+async def read_all_comment():
+    comments = CommentCRUD.read_all_comment()
+    if comments:
+        return [
+            CommentResponse(
+                commenter_id=comment.commenter_id,
+                comment_time=comment.comment_time,
+                content=comment.content,
+                comment_id=comment.comment_id,
+            )
+            for comment in comments
+        ]
+    else:
+        raise HTTPException(status_code=404, detail='No comments found')
+
+
+@router.get('/{commenter_id}', response_model=List[CommentResponse], status_code=status.HTTP_200_OK)
+async def read_comment_by_commenter(commenter_id: str):
+    "latest"
+    comments = CommentCRUD.read_comment_by_commenter(commenter_id)
+    if comments:
+        return [
+            CommentResponse(
+                commenter_id=comment.commenter_id,
+                comment_time=comment.comment_time,
+                content=comment.content,
+                comment_id=comment.comment_id,
+            )
+            for comment in comments
+        ]
+    else:
+        raise HTTPException(status_code=404, detail=f'No comments of {commenter_id} found')
+
+
+@router.delete('/{comment_id}', response_model=CommentResponse, status_code=status.HTTP_200_OK)
+async def delete_comment_by_id(comment_id: int):
+    comment = CommentCRUD.delete_comment_by_id(comment_id)
+    if comment:
+        return CommentResponse(
+            commenter_id=comment.commenter_id,
+            comment_time=comment.comment_time,
+            content=comment.content,
+            comment_id=comment.comment_id,
+        )
+    else:
+        raise HTTPException(status_code=404, detail=f"comment ID {comment_id} doesn't exist")
