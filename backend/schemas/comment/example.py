772362052156d07e49e945ef66416eeb1613f7fa
Dennis <e64112287@gs.ncku.edu.tw>
2024-11-29 17:37:32 +0800

    define a function to handle error response

--- backend/schemas/comment/example.py
+++ backend/schemas/comment/example.py
@@ -1,18 +1,40 @@
 from datetime import datetime
 
 
-class Config:
-    json_schema_extra = {
-        'example': {
-            'status': 'success',
-            'message': None,
-            'data': {
-                'comment_id': 1,
-                'commenter_id': 'dennislee03',
-                'content': 'Hello World!',
-                'comment_time': '2023-11-18T03:18:41.691',
-            },
-            'timestamp': '2023-11-18T13:42:22.308579',
-        }
-    }
+class CommentConfig:
     json_encoders = {datetime: lambda v: v.isoformat()}
+
+    EXAMPLE_1 = {
+        'comment_id': 1,
+        'commenter_id': 'dennislee03',
+        'content': 'Hello World!',
+        'comment_time': '2023-11-18T03:18:41.691',
+    }
+
+    EXAMPLE_2 = {
+        'comment_id': 2,
+        'commenter_id': 'dennislee03',
+        'content': 'Hello World!',
+        'comment_time': '2023-11-19T03:18:41.691',
+    }
+
+    EXAMPLE_3 = {
+        'comment_id': 3,
+        'commenter_id': 'dennislee03',
+        'content': 'Hello World!',
+        'comment_time': '2023-11-20T03:18:41.691',
+    }
+
+    RESPONSE_MODEL_EXAMPLE = {
+        'status': 'success',
+        'message': None,
+        'data': EXAMPLE_1,
+        'timestamp': '2023-11-18T13:42:22.308579',
+    }
+
+    LIST_RESPONSE_MODEL_EXAMPLE = {
+        'status': 'success',
+        'message': None,
+        'data': [EXAMPLE_1, EXAMPLE_2, EXAMPLE_3],
+        'timestamp': '2023-11-21T13:42:22.308579',
+    }
--- backend/schemas/common.py
+++ backend/schemas/common.py
@@ -4,6 +4,8 @@ from typing import Generic, Literal, Optional, TypeVar, Union
 
 from pydantic import BaseModel, Field
 
+from schemas.comment.example import CommentConfig
+
 T = TypeVar('T')
 
 
@@ -23,4 +25,11 @@ class CommentResponseModel(BaseModel, Generic[T]):
     status: Literal['success', 'error']
     message: Optional[str] = None
     data: Optional[T] = None
+    # TODO: adjust timestamp
     timestamp: Union[datetime, str] = datetime.now()
+
+    class Config:
+        # TODO: keys automatic sorting
+        json_encoder = CommentConfig.json_encoders
+        json_sort_keys = False
+        json_schema_extra = {'example': CommentConfig.RESPONSE_MODEL_EXAMPLE}
