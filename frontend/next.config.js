b07a456b906babdca3f2efe23885831c37424b87
owenowenisme <mses010108@gmail.com>
2024-12-10 01:13:06 +0800

    update compose.tml and dockerfile

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'standalone',
-    webpack: (config, { isServer }) => {
+    webpack: (config) => {
       config.resolve.alias['@'] = path.resolve(__dirname);
       return config;
     },
