3e9d309bf2991290de44cb841269e58542cc7ce6
Max042004 <sau525@gmail.com>
2024-12-19 18:03:40 +0800

    Refactor: delete unuse selectors, fix syntax error.

--- frontend/hooks/useAuthentication.ts
+++ frontend/hooks/useAuthentication.ts
@@ -3,15 +3,15 @@ import { useUserStore } from '@/global_state/user/store'; // Import Zustand stor
 
 export const useAuthentication = () =>{
 
-    const isAuthenticated = useUserStore(selectIsAuthenticated);
-    const { currentUser, login, logout } = useUserStore();
+  const isAuthenticated = selectIsAuthenticated(useUserStore());
+  const {currentUser, login, logout} = useUserStore();
 
-    // Handle the login button click
-    const handleLogin = () => {
-        if (!isAuthenticated) {
-        login(); // Simulate user login (you can replace this with actual login logic)
-        }
-    };
+  // Handle the login button click
+  const handleLogin = () => {
+      if (!isAuthenticated) {
+      login(); // Simulate user login (you can replace this with actual login logic)
+      }
+  };
 
   const handleLogout = () => {
     logout(); // Clear user data when logging out
