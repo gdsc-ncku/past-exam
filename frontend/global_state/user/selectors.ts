7b899779a8c8641ec4c66ab1fc4e7412461b6ade
Max042004 <sau525@gmail.com>
2024-12-19 18:03:40 +0800

    Refactor: delete unuse selectors, fix syntax error.

--- frontend/global_state/user/selectors.ts
+++ frontend/global_state/user/selectors.ts
@@ -1,15 +1,3 @@
 import { UserState } from './types';
 
-export const selectCurrentUser = (state: UserState) => state.currentUser;
-
 export const selectIsAuthenticated = (state: UserState) => !!state.currentUser;
-
-export const selectUserProfile = (state: UserState) => {
-  const user = state.currentUser;
-  return user ? {
-    userName: `${user.userName}`,
-    avatar: user.avatar,
-    email: user.email,
-    isProfileCompleted: user.isProfileCompleted,
-  } : null;
-};
