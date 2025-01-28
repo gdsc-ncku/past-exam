a00c1b4469c0fecb41e576674765441c68452e3a
Max042004 <sau525@gmail.com>
2025-01-28 17:00:04 +0800

    feature: finish reusable icon components

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,22 @@ const nextConfig = {
     output:'standalone',
     webpack: (config) => {
       config.resolve.alias['@'] = path.resolve(__dirname);
+
+      const fileLoaderRule = config.module.rules.find(
+        (rule) => rule.test && rule.test.test?.('.svg')
+      );
+  
+      if (fileLoaderRule) {
+        fileLoaderRule.exclude = /\.svg$/;
+      }
+  
+      // 2) Add a new rule for SVGR
+      config.module.rules.push({
+        test: /\.svg$/,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
+      
       return config;
     },
     experimental: {
