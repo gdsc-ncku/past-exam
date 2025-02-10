556cd27e21a466a9774d55c74ce4aefcfe3c1e60
Max042004 <157292081+Max042004@users.noreply.github.com>
2025-02-10 13:06:54 +0800

    Feature/upload icons (#26)
    * feat: upload svg icons
    
    * feature: finish reusable icon components
    
    * feat: uploads svg files: bookmark, bookmark-1, plus
    
    * chore: add new three svgs to iconsMap

---
+++ frontend/ui/icons.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { iconsMap, IconName } from 'src/assets/icons/iconsMap';  // your SVG imports
+import { colorMap } from 'styles/ColorMap';
+import { DesignSystemColor } from 'styles/ColorTokens';
+
+type IconProps = {
+  /** The name of the icon (must exist in iconsMap). */
+  name: IconName;
+  /** The color token from the design system. */
+  color?: DesignSystemColor;
+};
+
+/**
+ * Reusable Icon component
+ */
+export function Icon({
+  name,
+}: IconProps) {
+  const SvgIcon = iconsMap[name];
+
+  return (
+    <SvgIcon
+        aria-hidden="true"
+  />
+  );
+}
