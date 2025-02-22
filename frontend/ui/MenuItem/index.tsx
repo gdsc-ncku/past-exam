245c5a8003a752013d8398069b45787cd163d56a
owenowenisme <mses010108@gmail.com>
2025-02-22 18:37:20 +0800

    add onClick Event to MenuItem
    Signed-off-by: owenowenisme <mses010108@gmail.com>

--- frontend/ui/MenuItem/index.tsx
+++ frontend/ui/MenuItem/index.tsx
@@ -4,12 +4,19 @@ import clsx from 'clsx';
 interface MenuItemProps {
   children: React.ReactNode;
   icon?: React.ReactNode;
+  onClick?: () => void;
   className?: string;
 }
 
-export const MenuItem = ({ icon, children, className }: MenuItemProps) => {
+export const MenuItem = ({
+  icon,
+  children,
+  className,
+  onClick,
+}: MenuItemProps) => {
   return (
     <div
+      onClick={onClick}
       // If you want this to be purely non-interactive, remove hover/cursor classes
       className={clsx(
         'inline-flex items-center', // icon + text side by side
@@ -17,6 +24,7 @@ export const MenuItem = ({ icon, children, className }: MenuItemProps) => {
         'gap-2 px-4', // internal padding & space between icon/text
         'text-[16px] text-base text-secondary-700',
         'transition-colors duration-200 hover:bg-primary-50', // highlight on hover
+        'cursor-pointer',
         className,
       )}
     >
