23e4959d1a26f686586c2780a8295d99fe8e979a
owenowenisme <mses010108@gmail.com>
2025-02-23 15:52:45 +0800

    Revert "Dev/backend@0.0.0 (#5)"
    This reverts commit 316d9e38c4cfd66084ac2f2eb396dad4f4fea134.

--- backend/schemas/file.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class File(BaseModel):
-    filename: str  # The name of the file
-    uploader_id: int
--- backend/schemas/user.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class User(BaseModel):
-    username: str  # The name of the file
-    password: str
-    email: str
