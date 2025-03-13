f6afbf68f5b41dbd4d054c2de9743dd15be2a788
Max042004 <sau525@gmail.com>
2025-03-13 15:00:20 +0800

    Add space between spin and text

--- frontend/app/Loading.tsx
+++ frontend/app/Loading.tsx
@@ -6,6 +6,7 @@ export default function Loading() {
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="text-center">
         <div className="mx-auto h-16 w-16 animate-spin rounded-full border-b-4 border-t-4 border-primary-300"></div>
+        <div className="h-3"></div>
         <p className="text-small text-secondary">Loading...</p>
       </div>
     </div>
